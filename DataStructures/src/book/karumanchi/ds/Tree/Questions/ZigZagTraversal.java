7879d4463dbf204cf69248a176a2e030c4c95862
thaksu05 <Subrat.Thakur.consultant@nielsen.com>
2017-04-03 13:03:05 +0530

    ZigZagTree traversal

--- DataStructures/src/book/karumanchi/ds/Tree/Questions/ZigZagTraversal.java
+++ DataStructures/src/book/karumanchi/ds/Tree/Questions/ZigZagTraversal.java
@@ -9,13 +9,14 @@ import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
 
+import book.karumanchi.ds.Tree.BinarySearchTree;
 import book.karumanchi.ds.Tree.BinaryTreeNode;
 
 /**
  * @author subratt
  *
  */
-public class ZigZagTraversal extends BinaryTreeNode {
+public class ZigZagTraversal extends BinarySearchTree {
 
 	public ZigZagTraversal(int data) {
 		super(data);
@@ -47,53 +48,58 @@ public class ZigZagTraversal extends BinaryTreeNode {
 				
 	}
 	
-	public static void zigZagTraversalNonRecursive(BinaryTreeNode root,boolean isrightToLeft){
+	public static void zigZagTraversalNonRecursive(BinaryTreeNode root){
 		Queue<BinaryTreeNode> listTraversalQueue=new LinkedList<BinaryTreeNode>();
 		Stack<BinaryTreeNode> listTraversal=new Stack<BinaryTreeNode>();
 		if(root ==null){
 			return;
 		}
 		listTraversal.push(root);
-		while(!listTraversal.isEmpty() && isrightToLeft){
-			BinaryTreeNode btn=listTraversal.pop();
-			System.out.print(btn.getData() + "-> ");
-				if(btn.getLeft() !=null)
-				listTraversal.push(btn.getLeft());
-				if(btn.getRight() !=null)
-				listTraversal.push(btn.getRight());
-		}
-		while(!listTraversalQueue.isEmpty() && isrightToLeft){
-			BinaryTreeNode btn=listTraversalQueue.poll();
-			System.out.print(btn.getData() + "-> ");
-				if(btn.getLeft() !=null)
-					listTraversalQueue.offer(btn.getRight());
-				if(btn.getRight() !=null)
-					listTraversalQueue.offer(btn.getLeft());
+		while(!listTraversal.isEmpty() || !listTraversalQueue.isEmpty()){
 			
+			while(!listTraversal.isEmpty()){
+				BinaryTreeNode btn=listTraversal.pop();
+				System.out.print(btn.getData() + "-> ");
+				if(btn.getLeft() !=null)
+					listTraversalQueue.offer(btn.getLeft());
+				if(btn.getRight() !=null)
+					listTraversalQueue.offer(btn.getRight());
 			}
-			isrightToLeft = !isrightToLeft;
+
+			while( !listTraversalQueue.isEmpty()){
+				BinaryTreeNode btn=listTraversalQueue.poll();
+				System.out.print(btn.getData() + "-> ");
+					if(btn.getLeft() !=null)
+						listTraversal.push(btn.getLeft());
+					if(btn.getRight() !=null)
+						listTraversal.push(btn.getRight());
+				
+				}
 		}
+	}
 		
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		BinaryTreeNode root= new BinaryTreeNode(1);
-		BinaryTreeNode child11= new BinaryTreeNode(2);
-		BinaryTreeNode child12= new BinaryTreeNode(3);
-		BinaryTreeNode child111= new BinaryTreeNode(4);
-		BinaryTreeNode child1112= new BinaryTreeNode(5);
-		BinaryTreeNode child1111= new BinaryTreeNode (6);
-		root.setLeft(child11);
-		root.setRight(child12);
-		child11.setLeft(child111);
-		child111.setLeft(child1111);
-		child111.setRight(child1112);
+		BinaryTreeNode head=new BinaryTreeNode(10);
+		insert(head, 8);
+		insert(head, 9);
+		insert(head, 15);
+		insert(head, 7);
+		insert(head, 12);
+		insert(head, 16);
+		insert(head, 6);
+		insert(head, 13);
+		insert(head, 11);
 		//System.out.println(getHeight(root));
         //System.out.println("Zigzag Tree traversal result (Recursive) :");
-        zigZagTraversalRecursive(root,0,true);
-        zigZagTraversalNonRecursive(root,true);
+		//10-8-15-16-12-9-7-6-11-13
+		System.out.println("Inorder" + getInorderTraversal(head, null));
+        zigZagTraversalRecursive(head,0,true);
+        System.out.println("NonRecursive");
+        zigZagTraversalNonRecursive(head);
 	}
 
 }
