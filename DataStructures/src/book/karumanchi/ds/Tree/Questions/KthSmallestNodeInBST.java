3c5d89692fe67055718bca20d39628cd603f2f18
thaksu05 <Subrat.Thakur.consultant@nielsen.com>
2017-04-04 12:23:01 +0530

    KthSmallestNodeInBSTNOnRecurssive

---
+++ DataStructures/src/book/karumanchi/ds/Tree/Questions/KthSmallestNodeInBST.java
@@ -0,0 +1,52 @@
+package book.karumanchi.ds.Tree.Questions;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import book.karumanchi.ds.Tree.BinarySearchTree;
+import book.karumanchi.ds.Tree.BinaryTreeNode;
+
+public class KthSmallestNodeInBST extends BinarySearchTree{
+	
+	private int k=0;
+	public KthSmallestNodeInBST(int data) {
+		super(data);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		BinaryTreeNode head=new BinaryTreeNode(10);
+		insert(head, 8);
+		insert(head, 9);
+		insert(head, 15);
+		insert(head, 7);
+		insert(head, 12);
+		insert(head, 16);
+		insert(head, 6);
+		System.out.println(getInorderTraversal(head, null));
+		int k=2;
+		Queue sortedData=getKthSmallestNodeNonRecurssive(head,null);
+		while(k>1){
+			sortedData.poll();
+			k--;
+		}
+		System.out.println(sortedData.poll());
+	}
+	
+	private static Queue getKthSmallestNodeNonRecurssive(BinaryTreeNode head,Queue<Integer> sortedData){
+		if(sortedData==null){
+			sortedData= new LinkedList<Integer>();
+		}
+		
+		if(head==null ){
+			return null;
+		}
+		getKthSmallestNodeNonRecurssive(head.getLeft(), sortedData);	
+		sortedData.offer(head.getData());	
+		getKthSmallestNodeNonRecurssive(head.getRight(), sortedData);	
+		
+		return sortedData;
+	}
+
+}
